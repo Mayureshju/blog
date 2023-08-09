decac13a6c8b98c00691c706fe2a5ff09816984c
Mayuresh Juvekar <Juvekarmayuresh37@gmail.com>
2023-08-09 15:59:13 -0700

    final

---
+++ config/site.ts
@@ -0,0 +1,28 @@
+export interface SiteConfig {
+  siteName: string;
+  description: string;
+  currentlyAt: string;
+  socialLinks: {
+    twitter: string;
+    youtube: string;
+    github: string;
+    linkedin: string;
+    instagram: string;
+  };
+}
+
+const siteConfig: SiteConfig = {
+  siteName: "Explorer",
+  description:
+    "A minimal and lovely travel blog which shares experiences and citiest around the world!",
+  currentlyAt: "India",
+  socialLinks: {
+    twitter: "#",
+    youtube: "#",
+    github: "#",
+    linkedin: "#",
+    instagram: "#",
+  },
+};
+
+export default siteConfig;
